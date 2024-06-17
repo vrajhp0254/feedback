6d8b9908f39f91d5a4ab2cbbcf5040be3cb51b95
patel_vraj_48808 <vrajhp0254@gmail.com>
2024-06-17 13:14:08 +0530

    Update mongodb dependency to version 6.7.0

---
+++ lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject={
+
+}
+
+async function dbConnect(): Promise<void>{
+    if(connection.isConnected){
+        console.log("Using existing connection")
+        return;
+    }
+    try{
+        const db= await mongoose.connect(process.env.MONGODB_URI || "", {
+    })
+    connection.isConnected=db.connections[0].readyState;
+    console.log("New connection created")
+}
+catch(err){
+    console.log("error connecting to db", err)
+    process.exit(1)
+}}
+
+export default dbConnect;

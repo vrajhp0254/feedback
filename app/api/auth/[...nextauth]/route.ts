5079d0f9d4af76ae0861f1b8a8283a8f8baf19cf
patel_vraj_48808 <vrajhp0254@gmail.com>
2024-06-26 16:14:59 +0530

    chore: Refactor authentication option handling for NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,3 +2,4 @@ import NextAuth from "next-auth/next";
 import {authOptions} from "./option";
 
 const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST };
--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,26 +1,23 @@
 import mongoose from "mongoose";
 type ConnectionObject = {
-    isConnected?: number;
-}
+  isConnected?: number;
+};
 
-const connection: ConnectionObject={
+const connection: ConnectionObject = {};
 
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Using existing connection");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONDODB_URI || "", {});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("New connection created");
+  } catch (err) {
+    console.log("Error connecting to database from application\n", err);
+    process.exit(1);
+  }
 }
-
-async function dbConnect(): Promise<void>{
-    if(connection.isConnected){
-        console.log("Using existing connection")
-        return;
-    }
-    try{
-        const db= await mongoose.connect(process.env.MONGODB_URI || "", {
-    })
-    connection.isConnected=db.connections[0].readyState;
-    console.log("New connection created")
-}
-catch(err){
-    console.log("error connecting to db", err)
-    process.exit(1)
-}}
 
 export default dbConnect;

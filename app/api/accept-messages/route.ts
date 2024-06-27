9274affd69b99405719ddc2d77f2315a251c2e92
patel_vraj_48808 <vrajhp0254@gmail.com>
2024-06-27 20:23:06 +0530

    Refactor authentication option handling for NextAuth

---
+++ app/api/accept-messages/route.ts
@@ -0,0 +1,106 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { User } from "next-auth";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!user || !session?.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessage } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessage },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to update user status to accept messages",
+        },
+        { status: 401 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: true,
+          message: "Message acceptance status updated succesfully",
+        },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.log("Failed to update user status to accept messages");
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to update user status to accept messages",
+      },
+      { status: 401 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!user || !session?.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  try {
+    const foundUser = await UserModel.findById(userId);
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: true,
+          isAcceptingMessages: foundUser.isAcceptingMessage,
+        },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.log("Failed to update user status to accept messages");
+    return Response.json(
+      {
+        success: false,
+        message: "Error in getting message acceptance",
+      },
+      { status: 401 }
+    );
+  }
+}
--- app/api/auth/[...nextauth]/option.ts
+++ app/api/auth/[...nextauth]/option.ts
@@ -45,23 +45,23 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token }) {
-      if (token) {
-          session.user._id =token._id
-          session.user.isVerified= token.isVerified
-          session.user.isAcceptingMessage= token.isAcceptingMessage;
-          session.user.username = token.username;
-      }
-      return session;
-    },
     async jwt({ token, user }) {
       if (user) {
-        token._id = user._id?.toString()
+        token._id = user._id?.toString();
         token.isVerified = user.isVerified;
-        token.isAcceptingMessages= user.isAcceptingMessage;
+        token.isAcceptingMessages = user.isAcceptingMessage;
         token.username = user.username;
       }
       return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessage = token.isAcceptingMessage;
+        session.user.username = token.username;
+      }
+      return session;
     },
   },
   pages: {

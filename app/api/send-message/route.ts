da305f6a5dcc7e2e7b47944a6ff15ee9316a04be
patel_vraj_48808 <vrajhp0254@gmail.com>
2024-07-03 19:12:01 +0530

    solve issue in deploy

--- app/api/send-message/route.ts
+++ app/api/send-message/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: Request) {
       );
     }
     const newMessage = { content, createdAt: new Date() };
-    user.message.push(newMessage as Message);
+    user.messages.push(newMessage as Message);
     await user.save();
     return Response.json(
       {
